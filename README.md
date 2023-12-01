fc005aa9e58b28042b3ca29663b2ef9d4009faa0
bilal.s12 <bilal.s12@gmail.com>
2023-12-01 15:42:42 -0500

    modified script to store hashes of previously generated passwords. also uploaded readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # pWord
+
+this script generates a unique password of a specified length and estimates its strength using the `zxcvbn` library. 
+
+you will need:
+- ruby
+- `zxcvbn-ruby` gem
+
+install:
+`gem install zxcvbn-ruby`
+
+use:
+`ruby pWord.rb <password length>`
+
+if no argument is provided, the script defaults to a 12-character password. the output shows the generated password (cleartext) and its strength score. the score ranges from 0 (weak) to 4 (strong).
+
+
+# example
+`ruby pWord.rb 16`
+
+output:
+```
+generated password: 4ZgYKMbX3xvL2ZgY
+password strength: 3
+```
+
+the script also ensures that no 2 passwords repeat. it does this by storing the hashes of the passwords in a file called `passwords.txt`.
--- pWord.rb
+++ pWord.rb
@@ -1,5 +1,7 @@
 require 'zxcvbn'
 require 'securerandom'
+require 'set'
+require 'digest'
 
 def generate_password(length = 12)
 	SecureRandom.alphanumeric(length)
@@ -14,8 +16,36 @@ else
 	password_length = 12
 end
 
-password = generate_password
-puts "generated password: #{password}"
+# set that stores previously generated passwords
+password = Set.new
 
+# load previously generated password hashes from file
+if File.exist?('passwords.txt')
+	File.readlines('passwords.txt').each do |line|
+		passwords.add(line.strip)
+	end
+end
+
+# generate new password
+password = generate_password(password_length)
+
+# create hash of password
+password_hash = Digest::SHA256.hexdigest(password)
+
+# check if password hash has been generated before
+while passwords.include?(password_hash)
+	# generate new if it has been generated before
+	password = generate_password(password_length)
+	password_hash = Digest::SHA256.hexdigest(password)
+end
+
+
+# add new password hash to set of previously generated passwords
+passwords.add(password_hash)
+
+# write new password hash to file
+File.open('passwords.txt', 'a') { |file| file.puts(password_hash) }
+
+puts "generated password: #{password}"
 strength = Zxcvbn.test(password)
 puts "password strength: #{strength.score}"
