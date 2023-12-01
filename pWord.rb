f31426528c1ba226a526d25b5c08006b150a6d9b
bilal.s12 <bilal.s12@gmail.com>
2023-12-01 15:24:33 -0500

    accepting CLI inputs as args now

--- pWord.rb
+++ pWord.rb
@@ -5,6 +5,15 @@ def generate_password(length = 12)
 	SecureRandom.alphanumeric(length)
 end
 
+# check if argument was provided
+if ARGV[0]
+	# convert arg to integer and use it as password length
+	password_length = ARGV[0].to_i
+else
+	# default password length
+	password_length = 12
+end
+
 password = generate_password
 puts "generated password: #{password}"
 
