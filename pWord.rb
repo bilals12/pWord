86f3841677bc4545f275a2fdf6a553cf67a69a8f
bilal.s12 <bilal.s12@gmail.com>
2023-12-01 15:17:11 -0500

    added john the ripper strength check

--- pWord.rb
+++ pWord.rb
@@ -6,4 +6,9 @@ end
 
 password = generate_password
 puts "generated password: #{password}"
+
+# write password to file
 File.open('password.txt', 'w') { |f| f.puts(password) }
+
+# run john the ripper on password file
+system("john --format=Raw-MD5 --test password.txt")
